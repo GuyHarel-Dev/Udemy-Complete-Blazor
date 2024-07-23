521e6d5eeca12cb43d5b9a491511d3d45394692d
Guy Harel <GHarel@gestisoft.com>
2024-07-23 15:23:32 -0400

    Requis pour démarrage avec Authorization

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -13,7 +13,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddHttpClient();
 
