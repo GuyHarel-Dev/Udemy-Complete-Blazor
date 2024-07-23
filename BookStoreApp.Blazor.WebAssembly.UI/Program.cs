07844b83d410d34844839fb3fc3e286752d53e0f
Guy Harel <GHarel@gestisoft.com>
2024-07-23 15:12:16 -0400

    Ajout des pages du projet Server UI, et correctif Authorization dans program.cs

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -1,11 +1,32 @@
+using Blazored.LocalStorage;
 using BookStoreApp.Blazor.WebAssembly.UI;
+using BookStoreApp.Services;
+using BookStoreApp.Services.Interfaces;
+using BookStoreApp.Services.Providers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddHttpClient();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<BookStoreApiClientFactory>();
+builder.Services.AddSingleton<JwtSecurityTokenHandler>();
+
+builder.Services.AddScoped<IBookStoreApiService, BookStoreApiService>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(o => o.GetRequiredService<ApiAuthenticationStateProvider>());
+
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
